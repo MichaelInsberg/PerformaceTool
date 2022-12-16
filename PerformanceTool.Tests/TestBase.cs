fc23a8548c86ebbe3c55f338db082f217ac3674b
michael.insberg <michael.insberg@4micro.de>
2022-12-16 18:59:10 +0100

    Add new files

---
+++ PerformanceTool.Tests/TestBase.cs
@@ -0,0 +1,72 @@
+﻿namespace PerformanceTool.PerformanceTool.Tests
+{
+    /// <summary>
+    /// Defines the Test base.
+    /// </summary>
+    /// <typeparam name="T">Where T can be anything</typeparam>
+    public abstract class TestBase<T> : TestFixtureBase
+    {
+        private CancellationTokenSource cancellationTokenSource;
+
+        /// <summary>
+        /// Gets the object to test.
+        /// </summary>
+        protected T ObjectToTest { get; private set; }
+
+
+        /// <inheritdoc />
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            Identifier = Guid.NewGuid();
+            CreateApplicationCancellationToken();
+            InitializeTestObjects();
+            ObjectToTest = CreateTestObject();
+        }
+
+        private void CreateApplicationCancellationToken()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        /// <inheritdoc />
+        protected override void Dispose(bool dispose)
+        {
+            base.Dispose(dispose);
+
+            if (dispose)
+            {
+                if (cancellationTokenSource != null)
+                {
+                    cancellationTokenSource.Cancel();
+                    cancellationTokenSource.Dispose();
+                }
+
+                cancellationTokenSource = null;
+
+                var obj = ObjectToTest as IDisposable;
+                obj?.Dispose();
+                ObjectToTest = default;
+            }
+        }
+
+        /// <summary>
+        /// The object to test initialization not null.
+        /// </summary>
+
+        /// <summary>
+        /// The create test object.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
+        protected abstract T CreateTestObject();
+
+        protected virtual void InitializeTestObjects()
+        {
+            // 23.05.2022 MInsberg  : do nothing
+        }
+
+    }
+}
---
+++ PerformanceTool.Tests/TestFixtureBase.cs
@@ -0,0 +1,109 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+namespace PerformanceTool.PerformanceTool.Tests
+{
+    [TestFixture]
+    public abstract class TestFixtureBase : IDisposable
+
+    {
+        private ConsoleTraceListener tracer;
+
+
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        protected Guid Identifier { get; set; }
+
+        /// <summary>
+        /// Called when [time setup].
+        /// </summary>
+        [OneTimeSetUp]
+        public virtual void OneTimeSetup()
+        {
+            tracer = new ConsoleTraceListener();
+            _ = Trace.Listeners.Add(tracer);
+        }
+
+        /// <summary>
+        /// Called when [time tear down].
+        /// </summary>
+        [OneTimeTearDown]
+        public virtual void OneTimeTearDown()
+        {
+            Trace.Flush();
+            WriteDebugMessage($"OneTime teardown test {GetTestName()}");
+            if (tracer != null)
+            {
+                Trace.Listeners.Remove(tracer);
+            }
+        }
+
+        /// <summary>
+        /// The test setup.
+        /// </summary>
+        [SetUp]
+        public virtual void SetUp()
+        {
+            WriteDebugMessage($"Setup test {GetTestName()}");
+            Identifier = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// The tear down.
+        /// </summary>
+        [TearDown]
+        public virtual void TearDown()
+        {
+            WriteDebugMessage($"TearDown test {GetTestName()}");
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(dispose: true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Writes the debug message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public static void WriteDebugMessage(string message)
+        {
+            if (Debugger.IsAttached)
+            {
+                Debug.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        /// <summary>
+        /// The get test name method
+        /// </summary>
+        /// <param name="fullName"></param>
+        /// <returns>The test name</returns>
+        protected static string GetTestName(bool fullName = false)
+        {
+            return fullName ? TestContext.CurrentContext.Test.FullName : TestContext.CurrentContext.Test.Name;
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="dispose"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool dispose)
+        {
+            if (dispose)
+            {
+                tracer?.Dispose();
+                tracer = null;
+            }
+        }
+    }
+}
